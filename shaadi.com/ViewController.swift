224349e4e4cda5437ee383f686bee6ef75e1a747
janvi kachchhi007 <janukachchhi2@gmail.com>
2023-05-31 12:30:02 +0530

    Initial Commit

---
+++ shaadi.com/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shaadi.com
+//
+//  Created by R94 on 31/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appLogo: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
